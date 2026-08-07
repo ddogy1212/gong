0c6a61cbb9cb3f2edeabfd2e12852e8dbaa606b0
ddogy1212 <ddogy503@gmail.com>
2026-08-07 20:57:00 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gonggame-v288-boss-visual-achievement-mastery30';
+const CACHE_NAME='gonggame-v289-achievement-floatplay-mr-event';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png'];
 
 self.addEventListener('install',event=>{
