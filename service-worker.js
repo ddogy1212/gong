828a40aa3b72d230d7d2fc333409715a43e58860
ddogy1212 <ddogy503@gmail.com>
2026-08-07 23:27:35 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gonggame-v292-mode-guide-tutorial';
+const CACHE_NAME='gonggame-v293-demon-contract-mode-tips-float-guard';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png'];
 
 self.addEventListener('install',event=>{
