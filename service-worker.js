3277f9c7a3416e85e4715f9731e5e54ac4940853
ddogy1212 <ddogy503@gmail.com>
2026-08-08 20:42:39 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='gonggame-v310-friend-battle-invite-modal-tutorial-once';
-const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png'];
+const CACHE_NAME='gonggame-v311-yeonwoo-daeun-daughter-cross';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png','./characters/kang_yeonwoo_v311.png','./characters/choi_daeun_v311.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
