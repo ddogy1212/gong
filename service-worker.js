82d366aa78b0e07fa2a5dd7bb2a6a5c1b8414a6a
ddogy1212 <ddogy503@gmail.com>
2026-08-09 10:15:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gonggame-v325-taerin-ui-mastery-friend-stability';
+const CACHE_NAME='gonggame-v326-daughter-cross-compact-mastery-wall-rings';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png','./characters/kang_yeonwoo_v311.png','./characters/choi_daeun_v311.png','./characters/seoyeonseo_v316.png','./characters/kim_taerin_v323.jpg'];
 
 self.addEventListener('install',event=>{
