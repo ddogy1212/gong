f37e547cd7c5b616401004d310edc3eef562e826
ddogy1212 <ddogy503@gmail.com>
2026-08-09 12:58:54 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gonggame-v328-full-roster-attack-audit-core-bridge';
+const CACHE_NAME='gonggame-v329-taerin-attack-cap-x5';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png','./characters/kang_yeonwoo_v311.png','./characters/choi_daeun_v311.png','./characters/seoyeonseo_v316.png','./characters/kim_taerin_v323.jpg'];
 
 self.addEventListener('install',event=>{
