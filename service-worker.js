bcbad46934b4523850360c0ac453f21cd5702808
ddogy1212 <ddogy503@gmail.com>
2026-08-11 15:06:46 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gonggame-v351-mastery-recovery-uncapped';
+const CACHE_NAME='gonggame-v352-mastery-120-rewards-profile-fx';
 const CORE=['./index.html','./manifest.webmanifest','./icons/gonggame-192.png','./icons/gonggame-512.png','./icons/gonggame-maskable-512.png','./characters/lee_eunho.jpeg','./characters/yoon_juhyeong.jpg','./characters/lee_seohyun_v277.png','./characters/kang_yeonwoo_v311.png','./characters/choi_daeun_v311.png','./characters/seoyeonseo_v316.png','./characters/kim_taerin_v323.jpg','./characters/eom_haein_v343.png'];
 
 async function fresh(request){
